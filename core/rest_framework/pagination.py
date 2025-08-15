cc9d4ec59d9c44cc588ed8024b569e522abb46d4
Harol-mjl <harolmijaeljorgelopez@outlook.es>
2025-08-16 00:49:03 +0200

    chore: improve pagination

--- core/rest_framework/pagination.py
+++ core/rest_framework/pagination.py
@@ -4,8 +4,10 @@ class OptionalPageNumberPagination(pagination.PageNumberPagination):
     page_size = 10
 
     def paginate_queryset(self, queryset, request, view=None):
-        query_params = request.query_params
-        self.page_size = query_params.get("pagesize") or query_params.get("page_size") or self.page_size
-        if request.query_params.get("pagination") == "false":
+        query_params_lower = {k.lower(): v for k, v in request.query_params.items()}
+        query_param_pagination = query_params_lower.get("pagination")
+
+        self.page_size = query_params_lower.get("pagesize") or query_params_lower.get("page_size") or self.page_size
+        if query_param_pagination and query_param_pagination.strip().lower() == 'false':
             return None
         return super().paginate_queryset(queryset, request, view)
