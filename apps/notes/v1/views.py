6fc38fde62c4efbb0864afefe2be8b00655b6c2c
Harol-mjl <harolmijaeljorgelopez@outlook.es>
2025-08-17 22:15:23 +0200

    feat: add ordering, default ordering and search in NotesViewSet

--- apps/notes/v1/views.py
+++ apps/notes/v1/views.py
@@ -1,5 +1,6 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from apps.notes.models.notes import Notes
 from apps.notes.v1.filters import NotesFilter
@@ -10,4 +11,8 @@ class NotesViewSet(viewsets.ModelViewSet):
     queryset = Notes.objects.all()
     serializer_class = NoteSerializer
     filterset_class = NotesFilter
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+
+    search_fields = ('id', 'title',)
+    ordering_fields = ('id', 'type', 'updated_at', 'created_at')
+    ordering = ('-updated_at', )
